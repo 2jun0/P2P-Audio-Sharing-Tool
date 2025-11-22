cee55cbb5ee8d061ca26d7139c8b1e413e45d5f6
2jun0 <soo28819@gmail.com>
2025-11-22 23:23:47 +0900

    refactor: move macos dir to root

--- gstreamer.bash
+++ gstreamer.bash
@@ -1,10 +1,7 @@
 # This is dummy to learn gstreamer
 
 # Sender
-gst-launch-1.0 -v \
-audiotestsrc ! audioconvert ! audioresample ! opusenc ! rtpopuspay ! udpsink host=192.168.0.100 port=5002
+gst-launch-1.0 -v audiotestsrc ! audioconvert ! audioresample ! opusenc ! rtpopuspay ! udpsink host=127.0.0.1 port=61806
 
 # Receiver
-gst-launch-1.0 -v \
-udpsrc port=5002 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)OPUS" ! \
-rtpopusdepay ! opusdec ! audioconvert ! audioresample ! autoaudiosink
+gst-launch-1.0 -v udpsrc port=0 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)OPUS" ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! autoaudiosink
