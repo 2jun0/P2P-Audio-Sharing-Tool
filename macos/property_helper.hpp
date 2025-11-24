41eccd679ef3861e9c8f5f540ee66363f95da222
2jun0 <soo28819@gmail.com>
2025-11-25 01:48:27 +0900

    refactor: fix deprecated key

--- macos/property_helper.hpp
+++ macos/property_helper.hpp
@@ -1,5 +1,5 @@
 #include <CoreAudio/AudioHardware.h>
 #include <string>
 
-AudioObjectPropertyAddress getPropertyAddress(AudioObjectPropertySelector selector, AudioObjectPropertyScope scope = kAudioObjectPropertyScopeGlobal, AudioObjectPropertyElement element = kAudioObjectPropertyElementMaster);
+AudioObjectPropertyAddress getPropertyAddress(AudioObjectPropertySelector selector, AudioObjectPropertyScope scope = kAudioObjectPropertyScopeGlobal, AudioObjectPropertyElement element = kAudioObjectPropertyElementMain);
 std::string getStringProperty(AudioObjectID deviceID, AudioObjectPropertySelector selector);
